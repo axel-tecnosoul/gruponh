2dec8fd205cfe2c41f02679b69daec74310bfc1b
Axel Britzius <axelbritzius@gmail.com>
2026-02-26 09:36:49 -0300

    Fix typo in estados_compra table name in consultas.sql

--- consultas.sql
+++ consultas.sql
@@ -4,6 +4,6 @@ UPDATE computos_detalle SET reservado = 0 WHERE reservado < 0; -- CORRECCIÓN 1:
 INSERT INTO parametros (id, parametro, valor) VALUES (6, 'E-MAIL port', 587); -- CORRECCIÓN 2: Agregar el puerto de correo electrónico a la tabla parametros para el envío de emails.
 
 
-INSERT INTO estados_compras (id, estado) VALUES (5, 'Superado'); -- Nuevo estado para las compras.
+INSERT INTO estados_compra (id, estado) VALUES (5, 'Superado'); -- Nuevo estado para las compras.
 
 ejecutar el .php llamado fix_nro_oc.php, http://localhost/gruponh/fix_nro_oc.php?ejecutar=1 para que modifique http://localhost/gruponh/fix_nro_oc.php para ver los cambios que haría
